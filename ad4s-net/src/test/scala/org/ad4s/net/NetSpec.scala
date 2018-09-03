efc65c78bcfcea84c385ddf43c1ca4cee9d10d79
Fabian Murariu <murariu.fabian@gmail.com>
2018-09-03 21:47:56 +0100

    linear model in NetSpec

--- ad4s-net/src/test/scala/org/ad4s/net/NetSpec.scala
+++ ad4s-net/src/test/scala/org/ad4s/net/NetSpec.scala
@@ -1,8 +1,7 @@
 package org.ad4s.net
 
-import cats.implicits._
 import org.ad4s.core.Bv
-import org.ad4s.core.tape.BackpropContext
+import org.ad4s.core.tape.{BackpropContext, Tape}
 import org.scalatest.{FlatSpec, Matchers}
 
 class NetSpec extends FlatSpec with Matchers {
@@ -10,10 +9,9 @@ class NetSpec extends FlatSpec with Matchers {
   import BackpropContext.Implicits._
   import Bv.Implicits._
   import org.ad4s.core.Ops._
+  import org.ad4s.core.tape.TapeEvaluatorMagnet.Implicits._
 
-
-  "Net" should "take any foldable and update the variables via the optimizer" in pendingUntilFixed {
-    import org.ad4s.net.Net.Implicits._
+  "Net" should "take any foldable and update the variables via the gradient descent" in {
 
     val X = Vector[Double](3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167,
       7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1)
@@ -21,12 +19,30 @@ class NetSpec extends FlatSpec with Matchers {
     val Y = Vector[Double](1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221,
       2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3)
 
-    val cost = (W: Bv[Double], b: Bv[Double]) => (x: Bv[Double], y: Bv[Double]) => { implicit BC: BackpropContext[Double] =>
+    val cost = (W: Bv[Double], b: Bv[Double], x: Bv[Double], y: Bv[Double]) => { implicit BC: BackpropContext[Double] =>
       val pred = W * x + b
-      val J = pow(pred - y, Bv(2d)) // cost
-      J
+      // cost
+      pow(pred - y, Bv(2d))
     }
 
-    Net.run(X.zip(Y))(cost)
+    def repeat[T](n: Int, xs: Traversable[T]): Stream[T] = {
+      Stream.continually(xs).take(n).flatten
+    }
+
+    // behold SGD with batch_size = 1
+    val (weight, bias, _, c) = repeat(9000, X.zip(Y)).zipWithIndex.foldLeft((0.5, 0.5, 0.0001, 0.001)) {
+      case ((w, b, lr, _), ((x, y), step)) =>
+        val (z, (dw, db, _, _)) = Tape.runGrads(cost)((w, b, x, y))
+
+        val newW = w - (lr * dw)
+        val newB = b - (lr * db)
+        (newW, newB, lr, z)
+    }
+    // from one of the Tensorflow runs
+    // Epoch: 1000 cost= 0.077266559 W= 0.259104 b= 0.733052
+    c should === (0.077 +- 0.01)
+    weight should === (0.25 +- 0.01)
+    bias should === (0.8 +- 0.01)
+
   }
 }
